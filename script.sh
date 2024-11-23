63275e3e104b18f5d8f9ac1da8a35eaf318f65b4
ValeriaSilvaX <silvadiazvaleria@gmail.com>
2024-11-23 18:47:13 -0300

    Prueba para ver con que usuario se sube

--- script.sh
+++ script.sh
@@ -1,54 +1,29 @@
 #!/bin/bash
 
 # Ruta donde se encuentra el proyecto Terraform
-RUTA_TERRAFORM="/root/.ssh/Obligatorio_Cloud2024/terraform"
 
+RUTA_TERRAFORM="/root/.ssh/Obligatorio_Cloud2024/terraform"
 
 # Cambiar al directorio especificado
 if cd "$RUTA_TERRAFORM"; then
     echo "Entrando a la ruta $RUTA_TERRAFORM"
 
-    # Ejecutar terraform init y verificar si fue exitoso
-    if terraform init; then
-        echo "terraform init completado exitosamente."
-    else
-        echo "Error al ejecutar terraform init."
-        exit 1
-    fi
-
-    # Ejecutar terraform plan y verificar si fue exitoso
-    if terraform plan; then
-        echo "terraform plan completado exitosamente."
-    else
-        echo "Error al ejecutar terraform plan."
-        exit 1
-    fi
-
-    # Ejecutar terraform apply y verificar si fue exitoso
-       error_msg=$(terraform apply -auto-approve 2>&1)
-    if [[ "$error_msg" == *"AccessDenied: User: arn:aws:sts::"*": is not authorized to perform: iam:CreateOpenIDConnectProvider"* ]]; then
-        echo "Advertencia: Terraform apply falló debido a permisos insuficientes para crear un IAM OIDC Provider. Este paso será ignorado."
-    elif [[ "$error_msg" != "" ]]; then
-        echo "Error al ejecutar terraform apply: $error_msg"
-        exit 1
-    else
-        echo "terraform apply completado exitosamente."
-    fi
-else
-    echo "No se pudo acceder a la ruta $RUTA_TERRAFORM"
-    exit 1
-fi
-
-
-
+    # Ejecutar terraform init
+    echo "Iniciando terraform init..."
+    terraform init
+    
+    # Ejecutar terraform plan
+    echo "Ejecutando terraform plan..."
+    terraform plan
+    
+    # Ejecutar terraform apply y capturar la salida de error
+    echo "Ejecutando terraform apply..."
+    erraform apply -auto-approve 
+    
+ 
 # Configurar el contexto de Kubernetes para el cluster EKS
 echo "Configurando contexto de Kubernetes para EKS..."
-if aws eks update-kubeconfig --name terraform-eks --region us-east-1; then
-    echo "Configuración de kubeconfig completada."
-else
-    echo "Error al configurar kubeconfig para EKS."
-    exit 1
-fi
+    aws eks update-kubeconfig --name terraform-eks --region us-east-1
 
 #busca todos los archivos en el directorio manifietosd y arma los pods
 for i in "/root/.ssh/Obligatorio_Cloud2024/manifiestos/"; do
@@ -60,3 +35,7 @@ url=$(kubectl get svc --selector= frontend-external | cut -d " " -f 10 | grep "e
 echo "la url para la pagina es: "
 #Imprime el valor del LB
 echo "$url"
+else
+    echo "No se pudo acceder a la ruta $RUTA_TERRAFORM"
+    exit 1
+fi
