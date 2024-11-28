7ddd3da2e8f0106b51bcb3fbb4bc88ecd57c1de1
F-Olveira <f.olveira02@gmail.com>
2024-11-27 23:24:36 -0300

    Commit provider
    Creacion de provider para kubectl, para poder automatizar desde terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,3 +3,18 @@ provider "aws" {
   profile = var.perfil
 }
 
+terraform {
+  required_version = ">= 0.13"
+ 
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
+ 
+provider "kubectl" {
+  config_path = "~/.kube/config"
+   
+}
